ec6e01cd9c2305fe94fde3c8bea730a674dc49a1
Dvin Avanesian <dvin.av.04@gmail.com>
2022-07-22 00:07:15 +0430

    Revert

---
+++ src/server/server.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from 'express'
+const app = express()
+const port = 2022
+
+app.get('/', (req, res) => {
+	if (process.argv.slice(2)[0] == 'dev') {
+		app.set('view engine', 'pug')
+		res.render(process.cwd() + '/src/client/views/index.pug')
+	} else if (process.argv.slice(2)[0] == '') {
+		res.sendFile(__dirname + '/public/index.html')
+	}
+})
+
+app.use(express.static(process.cwd() + '/public'))
+app.use('/jquery', express.static(process.cwd() + '/node_modules/jquery/'))
+app.use('/mdc', express.static(process.cwd() + '/node_modules/material-components-web/'))
+app.use('/material-icons', express.static(process.cwd() + '/node_modules/material-icons/'))
+
+app.listen(port, () => {
+	console.log(`App listening on port ${port}`)
+})
